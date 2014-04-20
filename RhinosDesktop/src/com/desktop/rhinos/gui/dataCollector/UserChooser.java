c053f4f3417f96c736b437e2f3b499a4d7463f53
pedrop225 <pedrop225@gmail.com>
2014-04-20 00:05:51 +0000

    CurrentUserChooser renamed to UserChooser

--- RhinosDesktop/src/com/desktop/rhinos/gui/dataCollector/UserChooser.java
+++ RhinosDesktop/src/com/desktop/rhinos/gui/dataCollector/UserChooser.java
@@ -4,6 +4,8 @@ import javax.swing.JDialog;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 import javax.swing.JPanel;
 import javax.swing.JTree;
@@ -11,11 +13,11 @@ import javax.swing.JLabel;
 
 import com.desktop.rhinos.connector.MySqlConnector.App;
 
-public class CurrentUserChooser extends JDialog {
+public class UserChooser extends JDialog {
 
 	private static final long serialVersionUID = 3049466173724139108L;
 
-	public CurrentUserChooser() {
+	public UserChooser() {
 		getContentPane().setLayout(new BorderLayout());
 		
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -29,7 +31,20 @@ public class CurrentUserChooser extends JDialog {
 		getContentPane().add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new BorderLayout());
 		
-		JTree userTree = new JTree();
+		String [][] t= {{"A"}, {"D", "C"}, {"E"}};
+		Hashtable<String, String[]> h = new Hashtable<String, String[]>();
+		h.put("Admin", t[0]);
+		h.put("A", t[1]);
+		h.put("B", t[2]);
+//		h.put();
+		JTree userTree = new JTree(h);
 		panel_1.add(userTree);
+	}
+	
+	public static void main(String[] args) {
+		UserChooser ch = new UserChooser();
+		ch.setSize(540, 480);
+		
+		ch.setVisible(true);
 	}
 }
